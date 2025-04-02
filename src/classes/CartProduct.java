a52f8f6bb6b9ef1a4ceeba355284737fb34b71f2
sergei <adrian.illumina@gmail.com>
2025-04-02 21:55:55 +0700

    Added amount to constructor for derivatives

--- src/classes/CartProduct.java
+++ src/classes/CartProduct.java
@@ -6,4 +6,8 @@ public class CartProduct extends DerivativeProduct {
     public CartProduct(StoreProduct storeProduct) {
         super(storeProduct);
     }
+
+    public CartProduct(StoreProduct storeProduct, int amount) {
+        super(storeProduct, amount);
+    }
 }
--- src/classes/TransactionProduct.java
+++ src/classes/TransactionProduct.java
@@ -6,4 +6,8 @@ public class TransactionProduct extends DerivativeProduct {
     public TransactionProduct(StoreProduct storeProduct) {
         super(storeProduct);
     }
+
+    public TransactionProduct(StoreProduct storeProduct, int amount) {
+        super(storeProduct, amount);
+    }
 }
