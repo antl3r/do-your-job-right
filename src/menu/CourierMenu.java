fa14c896f9ac949ea0145992b98a8c1a04a70a24
sergei <adrian.illumina@gmail.com>
2025-04-02 17:55:39 +0700

    Merged

--- src/menu/CourierMenu.java
+++ src/menu/CourierMenu.java
@@ -1,15 +1,7 @@
 package menu;
 
 import abstracts.Menu;
-import classes.BurhanPedia;
-import classes.SharedScanner;
 
 public class CourierMenu extends Menu {
-    private final BurhanPedia burhanPedia;
-
-    public CourierMenu(BurhanPedia burhanPedia) {
-        super(); // Call the appropriate constructor of the Menu class
-        this.burhanPedia = burhanPedia;
-        // Use SharedScanner.getInstance() wherever a Scanner is needed
-    }
+    
 }
--- src/menu/SellerMenu.java
+++ src/menu/SellerMenu.java
@@ -1,9 +1,6 @@
 package menu;
 
 import abstracts.Menu;
-import classes.BurhanPedia;
-import classes.RouterItem;
-import classes.SharedScanner;
 
 public class SellerMenu extends Menu {
     private final BurhanPedia burhanPedia;
