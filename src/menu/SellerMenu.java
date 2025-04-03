077417384385e7a765bf5e6f955025fc3b7cac52
sergei <adrian.illumina@gmail.com>
2025-04-03 11:31:18 +0700

    Added handleCekSaldo

--- src/menu/SellerMenu.java
+++ src/menu/SellerMenu.java
@@ -5,19 +5,40 @@ import classes.RouterItem;
 import classes.Seller;
 
 public class SellerMenu extends UserMenu<Seller> {
+    private void handleCekSaldo() {
+        System.out.println("Saldo saat ini: " + user.getBalance());
+    }
+
     public SellerMenu(Seller user) {
         super(user);
         System.out.println("===== MENU PENJUAL =====");
         this.router.addRouterItems(
-            new RouterItem("Cek Produk", () -> {return false;}),
-            new RouterItem("Tambah Produk", () -> {return false;}),
-            new RouterItem("Tambah Stok", () -> {return false;}),
-            new RouterItem("Ubah Harga Barang", () -> {return false;}),
-            new RouterItem("Kirim Barang", () -> {return false;}),
-            new RouterItem("Lihat Laporan Pendapatan", () -> {return false;}),
-            new RouterItem("Cek Saldo", () -> {return false;}),
-            new RouterItem("Lihat Riwayat Transaksi", () -> {return false;}),
-            new RouterItem("Kembali ke Menu Utama", () -> {return false;})
-        );
+                new RouterItem("Cek Produk", () -> {
+                    return false;
+                }),
+                new RouterItem("Tambah Produk", () -> {
+                    return false;
+                }),
+                new RouterItem("Tambah Stok", () -> {
+                    return false;
+                }),
+                new RouterItem("Ubah Harga Barang", () -> {
+                    return false;
+                }),
+                new RouterItem("Kirim Barang", () -> {
+                    return false;
+                }),
+                new RouterItem("Lihat Laporan Pendapatan", () -> {
+                    return false;
+                }),
+                new RouterItem("Cek Saldo", () -> {
+                    return false;
+                }),
+                new RouterItem("Lihat Riwayat Transaksi", () -> {
+                    return false;
+                }),
+                new RouterItem("Kembali ke Menu Utama", () -> {
+                    return false;
+                }));
     }
 }
